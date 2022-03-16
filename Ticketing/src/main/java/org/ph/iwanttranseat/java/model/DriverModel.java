530a9990230be0a7f0aef33c68fe13789b981379
DiegorJo <DiegorJo@G07PHXNWES02961.g07.fujitsu.local>
2022-03-17 01:20:55 +0800

    Model > Variables, Getters && Setters.

--- Ticketing/src/main/java/org/ph/iwanttranseat/java/model/DriverModel.java
+++ Ticketing/src/main/java/org/ph/iwanttranseat/java/model/DriverModel.java
@@ -1,5 +1,21 @@
 package org.ph.iwanttranseat.java.model;
 
 public class DriverModel {
-
+	
+	private String driverFirstname = "";
+	private String driverLastname = "";
+	
+	
+	public String getDriverFirstname() {
+		return driverFirstname;
+	}
+	public void setDriverFirstname(String driverFirstname) {
+		this.driverFirstname = driverFirstname;
+	}
+	public String getDriverLastname() {
+		return driverLastname;
+	}
+	public void setDriverLastname(String driverLastname) {
+		this.driverLastname = driverLastname;
+	}	
 }
