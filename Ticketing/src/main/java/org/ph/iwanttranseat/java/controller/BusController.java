76b6c9a6d6ef37bbd7a035e83884073b8f451724
DiegorJo <DiegorJo@G07PHXNWES02961.g07.fujitsu.local>
2022-03-28 15:04:06 +0800

    [Update] Bus Controller servlet: URL.

--- Ticketing/src/main/java/org/ph/iwanttranseat/java/controller/BusController.java
+++ Ticketing/src/main/java/org/ph/iwanttranseat/java/controller/BusController.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.ph.iwanttranseat.java.dao.BusDAO;
 import org.ph.iwanttranseat.java.model.BusModel;
 
-@WebServlet("/")
+@WebServlet(urlPatterns = {"/busDetails", "/insertBusDetails", "/deleteBusDetails", "/editForm", "/updateBusDetails", "/busList"})
 public class BusController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	BusDAO busDAO;
