a7777189de416aa0bff4467fb08fd76be9e672c2
JabezJoshuaB <it.jabez16@gmail.com>
2022-03-28 16:27:20 +0800

    Update Datatable responsiveness

--- Ticketing/src/main/webapp/js/index_passenger.js
+++ Ticketing/src/main/webapp/js/index_passenger.js
@@ -1,16 +1,24 @@
-$(document).ready(function() {
-	$('#bus-schedule-table').DataTable({
+$(document).ready(function () {
+	let table = $('#bus-schedule-table').DataTable({
+		columnDefs: [
+			{ responsivePriority: 1, targets: 0 },
+			{ responsivePriority: 2, targets: -1 }
+		],
 		responsive: {
 			details: {
-				display: $.fn.dataTable.Responsive.display.modal({
-					header: function(row) {
-						var data = row.data();
-						return 'Details for ' + data[0] + ' ' + data[1];
-					}
-				}),
-				renderer: $.fn.dataTable.Responsive.renderer.tableAll({
-					tableClass: 'table'
-				})
+				renderer: function (api, rowIdx, columns) {
+					var anyHidden = false;
+					var data = $.map(columns, function (col, i) {
+						if (col.hidden) {
+							anyHidden = true;
+						}
+						var hidden = col.hidden ? true : !table.column(col.columnIndex).visible();
+						return hidden ? '<tr data-dt-row="' + col.rowIndex + '" data-dt-column="' + col.columnIndex + '">'
+						 + '<td>' + col.title + '</b></td> ' + '<td>' + table.cell(rowIdx, i).render('display') + '</td>' + '</tr>' : '';
+					}).join('');
+
+					return anyHidden ? $('<table/>').append(data) : false;
+				}
 			}
 		}
 	});
