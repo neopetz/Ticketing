a22d45796653962941c88c3e732c24230f3d3017
JabezJoshuaB <it.jabez16@gmail.com>
2022-03-29 13:23:04 +0800

    Update admin login redirecting to index admin

--- Ticketing/src/main/java/org/ph/iwanttranseat/java/dao/AdminDAO.java
+++ Ticketing/src/main/java/org/ph/iwanttranseat/java/dao/AdminDAO.java
@@ -1,5 +1,32 @@
 package org.ph.iwanttranseat.java.dao;
 
-public class AdminDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.ph.iwanttranseat.java.model.AdminModel;
+
+public class AdminDAO {
+	private String query = "SELECT * FROM admin WHERE username = ? AND password = ?;";
+	
+	public boolean validate(AdminModel adminModel) throws ClassNotFoundException {
+		
+		boolean haveAccess = false;
+		
+		try (Connection connection = JDBCUtils.getConnection();
+				PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+			
+			preparedStatement.setString(1, adminModel.getUsername());
+			preparedStatement.setString(2, adminModel.getPassword());
+			
+			System.out.println(preparedStatement);
+			ResultSet rs = preparedStatement.executeQuery();
+			haveAccess = rs.next();
+		} catch (SQLException e) {
+			JDBCUtils.printSQLException(e);
+		}
+		
+		return haveAccess;
+	}
 }
--- Ticketing/src/main/java/org/ph/iwanttranseat/java/model/AdminModel.java
+++ Ticketing/src/main/java/org/ph/iwanttranseat/java/model/AdminModel.java
@@ -2,20 +2,24 @@ package org.ph.iwanttranseat.java.model;
 
 public class AdminModel {
 	
-	private String admin;
+	private String username;
 	private String password;
 	
+	public String getUsername() {
+		return username;
+	}
 	
-	public String getAdmin() {
-		return admin;
+	public void setUsername(String username) {
+		this.username = username;
 	}
-	public void setAdmin(String admin) {
-		this.admin = admin;
-	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	
 }
